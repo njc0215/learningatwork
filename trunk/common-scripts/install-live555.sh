f9bb241fcee7a96e565d188f4abffcd41b786543
nguyen.anhquan <nguyen.anhquan@08b9c65a-88cd-11de-832f-d15e35d84da2>
2009-09-27 23:17:26 +0000

    Add scripts to install Live555 Media Server and test it with VLC

---
+++ trunk/common-scripts/install-live555.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+mkdir live555
+cd live555
+wget http://www.live555.com/mediaServer/linux/live555MediaServer
+
+echo "DOWNLOAD A SAMPLE MP3 FILE for Testing"
+wget --output-document=test.mp3 http://www.a1sounddownload.com/Halloween123/ghoullaugh.mp3
+
+echo "Execute the Live555MediaServer"
+live555MediaServer
